5a47de39e9d3acd62502afbef8956c24b0dc9845
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2023-09-16 00:01:37 -0300

    Update

---
+++ script.js
@@ -0,0 +1,5 @@
+// https://economia.awesomeapi.com.br/json/last/USD-BRL
+
+// {"USDBRL":{"code":"USD","codein":"BRL","name":"Dólar Americano/Real Brasileiro",
+// "high":"4.9194","low":"4.8821","varBid":"-0.0332","pctChange":"-0.68","bid":"4.8815",
+// "ask":"4.8827","timestamp":"1694696683","create_date":"2023-09-14 10:04:43"}}
